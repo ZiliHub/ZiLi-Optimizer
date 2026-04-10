75ec53a96b5aee081f10d8b3e1828467f87a4dbb
ZiliHub <hackgpo59@gmail.com>
2026-04-10 23:27:20 +0700

    Update index.lua

--- index.lua
+++ index.lua
@@ -9,9 +9,7 @@ if env.ZiliHub_Loaded then
     
     -- Dùng trick tạo Event ảo để ép dừng luồng script vĩnh viễn
     -- Đảm bảo 100% ngắt được script trên mọi loại Executor mà không văng lỗi đỏ
-    local freezeThread = Instance.new("BindableEvent")
-    freezeThread.Event:Wait() 
-    return 
+    return coroutine.yield()
 end
 
 env.ZiliHub_Loaded = true
